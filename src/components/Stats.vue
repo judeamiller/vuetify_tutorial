16be9c49b671202de52d98d9bf495da9d3fe13fc
Jude Baca-Miller <judeamiller@gmail.com>
2019-11-28 11:39:15 -0700

    create stats component

---
+++ src/components/Stats.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+    <v-card-title>{{ stat.title }}</v-card-title>
+    <v-card-text class="display-2">{{ stat.value }}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "StatCard",
+  props: {
+    stat: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
