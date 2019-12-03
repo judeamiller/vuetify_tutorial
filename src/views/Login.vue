bd8b83066447d6318a1e7b4c864e4c1ccaf4871c
Jude Baca-Miller <judeamiller@gmail.com>
2019-12-02 19:06:12 -0700

    trigger signedin on login btn

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -17,9 +17,9 @@
     </v-card-text>
     <v-divider></v-divider>
     <v-card-actions>
-      <v-btn color="success">Register</v-btn>
+      <v-btn to="/signup" color="success">Register</v-btn>
       <v-spacer></v-spacer>
-      <v-btn color="info">Login</v-btn>
+      <v-btn color="info" @click="login()">Login</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -31,6 +31,21 @@ export default {
     return {
       showPassword: false
     };
+  },
+  computed: {
+    signedIn: {
+      get() {
+        return this.$store.getters.signedIn;
+      },
+      set(newValue) {
+        this.$store.commit("setSignedIn", newValue);
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.signedIn = true;
+    }
   }
 };
 </script>
