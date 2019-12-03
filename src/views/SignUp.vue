e7d3c08f0a98a7736ad68fd2165c8274a6a7fac7
Jude Baca-Miller <judeamiller@gmail.com>
2019-12-02 18:42:34 -0700

    add new signup component

---
+++ src/views/SignUp.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Sign Up</h1>
+        <v-form>
+          <v-text-field v-model="email" label="Email" type="email"></v-text-field>
+          <v-autocomplete
+            label="Which Browser Are You Using?"
+            :items="browsers"
+            v-model="selectedBrowser"
+          ></v-autocomplete>
+          <v-text-field label="Birthday" readonly></v-text-field>
+          <v-date-picker v-model="birthday"></v-date-picker>
+          <v-checkbox label="Agree to terms and conditions." v-model="accept"></v-checkbox>
+        </v-form>
+      </v-col>
+    </v-row>
+    <v-snackbar v-model="showAlert">
+      You picked the ONLY wrong choice. Any other browser will do. May I suggest &nbsp;
+      <a
+        href="https://www.mozilla.org/en-US/firefox/new"
+        target="blank"
+      >Firefox</a>
+      <v-btn color="black" @click="selectedBrowser = null">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </v-snackbar>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "Signup",
+  data() {
+    return {
+      browsers: [
+        "Chrome",
+        "Firefox",
+        "Safari",
+        "Edge",
+        "Opera",
+        "Brave",
+        "Internet Explorer"
+      ],
+      selectedBrowser: null,
+      email: "",
+      birthday: "",
+      accept: false
+    };
+  },
+  computed: {
+    showAlert() {
+      if (this.selectedBrowser == "Internet Explorer") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+};
+</script>
