261e6c266a1c8bd9ac76fbeb438392881bcf8d83
Jude Baca-Miller <judeamiller@gmail.com>
2019-12-02 19:05:55 -0700

    add sign out

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@
           text
           rounded
           class="my-2"
+          :class="{'d-none': !link.show}"
         >{{ link.label }}</v-btn>
         <v-col class="primary lighten-2 py-4 text-center white--text xs12" cols="12">
           {{ new Date().getFullYear() }} —
@@ -30,25 +31,60 @@ export default {
   name: "App",
   components: { Navbar },
 
-  data: () => ({
-    links: [
-      {
-        label: "Home",
-        url: "/"
-      },
-      {
-        label: "About",
-        url: "/about"
-      },
-      {
-        label: "Login",
-        url: "/login"
-      },
-      {
-        label: "Dashboard",
-        url: "/dashboard"
+  data() {
+    return {
+      links: [
+        {
+          label: "Home",
+          url: "/",
+          show: true
+        },
+        {
+          label: "About",
+          url: "/about",
+          show: true
+        },
+        {
+          label: "Login",
+          url: "/login",
+          show: true
+        },
+        {
+          label: "Sign Up",
+          url: "/signup",
+          show: true
+        },
+        {
+          label: "Dashboard",
+          url: "/dashboard",
+          show: true
+        }
+      ]
+    };
+  },
+  computed: {
+    signedIn() {
+      return this.$store.getters.signedIn;
+    }
+  },
+
+  watch: {
+    signedIn(newData) {
+      console.log("newData is", newData);
+      if (newData === true) {
+        this.links.forEach(element => {
+          if (element.label == "Sign Up" || element.label == "Login") {
+            element.show = false;
+          }
+        });
+      } else {
+        this.links.forEach(element => {
+          if (element.label == "Sign Up" || element.label == "Login") {
+            element.show = true;
+          }
+        });
       }
-    ]
-  })
+    }
+  }
 };
 </script>
